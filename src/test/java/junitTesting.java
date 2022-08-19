c5c6a7488a4ab6c5e6897267f18c5af877e4fcee
Tammy <that0@allstate.com>
2022-08-19 17:49:45 +0000

    HR commit 4 with JUNITS

--- src/test/java/junitTesting.java
+++ src/test/java/junitTesting.java
@@ -1,4 +1,57 @@
+import java.time.LocalDate;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class junitTesting {
+private     Employee e;
 
+    @Test
+    public void testingFirstNameIsPresent() {
+        //given the employee
 
+        //when first name is missing
+        e = new Employee("12345", "Ms.", "", "harte", "54321", LocalDate.parse("2018-08-20") ,22222.d);
+
+        //then record is invalid
+        assertEquals(new Boolean(true), e.getfName().length() >0);
+    }
+
+    @Test
+    public void testForMissingFirstName() {
+    //given the employee
+
+    //when first name is missing
+        e = new Employee("12345", "Ms.", "", "harte", "54321", LocalDate.parse("2018-08-20") ,22222.d);
+        System.out.println(e.getfName().length());
+    //then record is invalid
+        assertEquals(new Boolean(true), e.getfName().length() <1);
+    }
+
+    @Test
+    public void testForMissingSurName() {
+        //given the employee
+        Employee e;
+
+        //when first name is missing
+        e = new Employee("12345", "Ms.", "", "harte", "54321", LocalDate.parse("2018-08-20") ,22222.d);
+
+        //then record is valid
+        assertEquals(new String("harte"), e.getsName());
+    }
+
+    @Test
+    public void testForValidSalaryAmount() {
+        //given the employee
+        Employee e;
+
+        //when first name is missing
+        e = new Employee("12345", "Ms.", "", "harte", "54321", LocalDate.parse("2018-08-20") ,0.d);
+
+        //then record is invalid
+        assertEquals(new Double(22222.d), e.getSalary());
+        e.setSalary(22222.d);
+        assertEquals(new Double(22222.d), e.getSalary());
+        assertEquals(new Boolean(false), (e.getSalary() > 0.0) );
+    }
 }
